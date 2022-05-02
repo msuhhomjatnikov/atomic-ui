b877bef66a6db85f0d7261c51f3866fdf79d1cf8
msukhomyatnikov <maksim.suhhomjatnikov@getmoss.com>
2022-05-02 17:30:53 +0200

    add action toast, config fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export * from './components/index';
+import './abstracts/Color/Color.sass';
+
+export { default as LoadingSpinner } from './components/LoadingSpinner';
+export { default as Input } from './components/Input';
+export { default as Checkbox, CHECKBOX_SIZE } from './components/Checkbox';
+export { default as Banner } from './components/Banner';
+export {
+    default as Button,
+    BUTTON_ICON,
+    BUTTON_ICON_POS,
+    BUTTON_SIZE,
+    BUTTON_TYPE,
+} from './components/Button';
+export { default as Switch } from './components/Switch';
+export { default as ActionToast } from './components/ActionToast';
+export {
+    default as StatusBadge,
+    STATUS_BADGE_COLOR,
+    STATUS_BADGE_ICON,
+} from './components/StatusBadge';
+export { default as Chip } from './components/Chip';
+export { default as SegmentedControls } from './components/SegmentedControls';
+export { default as AvatarImage } from './components/AvatarImage';
+export { default as SideNavigation, SIDE_NAVIGATION_STATE } from './components/SideNavigation';
+export {
+    default as FullScreenModal,
+    FullScreenModalContent,
+} from './components/Modal/FullScreenModal';
+export { default as HelpTooltip } from './components/HelpTooltip';
+export { default as PageCard } from './components/PageCard';
+export { default as RadioButton, RADIO_BUTTON_SIZE } from './components/RadioButton';
+
